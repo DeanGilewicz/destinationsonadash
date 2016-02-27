f87f9d35c4140d3151357ea0c0f1d56c90cb8dbc
Dean Gilewicz <gilewiczd@gmail.com>
2016-02-27 01:13:32 -0500

    set up the about page and commented out code for header

--- wp-content/themes/juliesjourneys/page.php
+++ wp-content/themes/juliesjourneys/page.php
@@ -20,22 +20,23 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			// get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
+			// if ( comments_open() || get_comments_number() ) {
+			// 	comments_template();
+			// }
+			the_content();
 			// End of the loop.
 		endwhile;
 		?>
 
 	</main><!-- .site-main -->
 
-	<?php get_sidebar( 'content-bottom' ); ?>
+	<?php // get_sidebar( 'content-bottom' ); ?>
 
 </div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
+<?php // get_sidebar(); ?>
+
 <?php get_footer(); ?>
