b0d066c4640dd9017b5a133c464b20fc230bf0d0
Dean Gilewicz <gilewiczd@gmail.com>
2016-04-17 00:31:44 -0400

    updated homepage slider layout and building out single post template and comments section

--- wp-content/themes/juliesjourneys/comments.php
+++ wp-content/themes/juliesjourneys/comments.php
@@ -22,58 +22,96 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+	<div class="row">
+		<div class="small-12 columns">
+
+
+
+			<?php if ( have_comments() ) : ?>
+				<h2 class="comments-title">
+					<?php
+						$comments_number = get_comments_number();
+						if ( 1 === $comments_number ) {
+							/* translators: %s: post title */
+							printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'juliesjourneys' ), get_the_title() );
+						} else {
+							printf(
+								/* translators: 1: number of comments, 2: post title */
+								_nx(
+									'%1$s thought on &ldquo;%2$s&rdquo;',
+									'%1$s thoughts on &ldquo;%2$s&rdquo;',
+									$comments_number,
+									'comments title',
+									'juliesjourneys'
+								),
+								number_format_i18n( $comments_number ),
+								get_the_title()
+							);
+						}
+					?>
+				</h2>
+
+				<?php the_comments_navigation(); ?>
+
+				<ol class="comment-list">
+					<?php
+						wp_list_comments( array(
+							'style'       => 'ol',
+							'short_ping'  => true,
+							'avatar_size' => 42,
+						) );
+					?>
+				</ol><!-- .comment-list -->
+
+				<?php the_comments_navigation(); ?>
+
+			<?php endif; // Check for have_comments(). ?>
+
 			<?php
-				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
-					/* translators: %s: post title */
-					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'juliesjourneys' ), get_the_title() );
-				} else {
-					printf(
-						/* translators: 1: number of comments, 2: post title */
-						_nx(
-							'%1$s thought on &ldquo;%2$s&rdquo;',
-							'%1$s thoughts on &ldquo;%2$s&rdquo;',
-							$comments_number,
-							'comments title',
-							'juliesjourneys'
-						),
-						number_format_i18n( $comments_number ),
-						get_the_title()
-					);
-				}
+				// If comments are closed and there are comments, let's leave a little note, shall we?
+				if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 			?>
-		</h2>
+				<p class="no-comments"><?php _e( 'Comments are closed.', 'juliesjourneys' ); ?></p>
+			<?php endif; ?>
 
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 42,
+
+				$commenter = wp_get_current_commenter();
+				$req = get_option( 'require_name_email' );
+				$aria_req = ( $req ? " aria-required='true'" : '' );
+
+				$fields =  array(
+
+					'author' =>
+						'<p class="comment-form-author"><label for="author">
+							<input id="author" name="author" type="text" placeholder="Name *" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
+						</p>',
+
+					'email' =>
+						'<p class="comment-form-email">
+							<input id="email" name="email" type="text" placeholder="Email *" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />
+						</p>'
+
+					// 'url' =>
+					// 	'<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+					// 	'<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+					// 	'" size="30" /></p>'
+				);
+
+				// Comment form for users to leave a comment
+				comment_form( array(
+					'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+					'title_reply'        => 'Leave a Comment',
+					'title_reply_after'  => '</h2>',
+					'comment_field' 	 => '<p class="comment-form-comment">
+		    									<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" placeholder="Comment *"></textarea>
+		    								</p>',
+		    		'fields' => apply_filters( 'comment_form_default_fields', $fields ),
 				) );
 			?>
-		</ol><!-- .comment-list -->
 
-		<?php the_comments_navigation(); ?>
+		</div>
 
-	<?php endif; // Check for have_comments(). ?>
-
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'juliesjourneys' ); ?></p>
-	<?php endif; ?>
-
-	<?php
-		comment_form( array(
-			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-			'title_reply_after'  => '</h2>',
-		) );
-	?>
+	</div>
 
 </div><!-- .comments-area -->
--- wp-content/themes/juliesjourneys/template-parts/content-single.php
+++ wp-content/themes/juliesjourneys/template-parts/content-single.php
@@ -9,35 +9,97 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<?php twentysixteen_excerpt(); ?>
+	<?php // twentysixteen_excerpt(); ?>
 
-	<?php twentysixteen_post_thumbnail(); ?>
+	<?php // twentysixteen_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<?php 
+		// echo '<pre>';
+		// print_r($post);
+		// print_r(get_post_meta(get_the_id()));
+		$postType = get_post_type_object(get_post_type());
+		// echo '</pre>'; 
+	?>
+
+	<div class="content-area post-single">
+		
+		<div class="row">
+
+			<div class="small-12 columns">
+				
+				<div class="post-meta-category-single">
+					<a href="/<?= $postType->labels->name; ?>"><?= $postType->labels->name; ?></a>
+				</div>
+			
+			</div>
+
+		</div>
+
+		<div class="row">
+
+			<div class="small-12 columns">
+
+				<a href="<?php the_permalink(); ?>" class="post-title">
+					<h2><?php the_title(); ?></h2>
+				</a>
+
+			</div>
+
+		</div>
+
+		<div class="row">
+			<div class="small-12 columns">
+				<span class="post-meta-date"><?php the_date('M j, Y'); ?></span>
+				<span class="post-meta-comments"><?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="small-12 columns sharebox">
+				
+				<a class="sharebox_btn heading_font" href="javascript:void(0);">
+					Share
+				</a>
+
+				<div class="sharebox_links">
+					<a class="social_link facebook" href="http://www.facebook.com/sharer.php?u=http://demo.evatheme.com/wp/voyager/demo/gallery-post-format-with-featured-images/&amp;t=Welcome to St-Yzans" title="Facebook" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><i class="fa fa-facebook"></i></a>
+					<a class="social_link twitter" href="http://twitter.com/home?status=Welcome to St-Yzans http://demo.evatheme.com/wp/voyager/demo/gallery-post-format-with-featured-images/" title="Twitter" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><i class="fa fa-twitter"></i></a>
+					<a class="social_link linkedin" href="https://www.linkedin.com/cws/share?url=http://demo.evatheme.com/wp/voyager/demo/gallery-post-format-with-featured-images/&amp;title=Welcome to St-Yzans&amp;summary=Luxury e-retail giants Net-a-Porter and Yoox are combining companies, Yoox Group announced this morning. The all-share deal comes after rumors that Amazon was interested in buying Net-a-Porter. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.Donec pede justo, fringilla vel, aliquet nec, […]" title="Linkedin Share" data-image="http://demo.evatheme.com/wp/voyager/demo/wp-content/uploads/2014/09/post22.jpg" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><i class="fa fa-linkedin"></i></a><script type="in/share" data-url="http://demo.evatheme.com/wp/voyager/demo/gallery-post-format-with-featured-images/" data-counter="right"></script>
+					<a class="social_link google-plus" href="http://plus.google.com/share?url=http://demo.evatheme.com/wp/voyager/demo/gallery-post-format-with-featured-images/&amp;title=Welcome+to+St-Yzans" title="Google+" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><i class="fa fa-google-plus"></i></a>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="row container-content">
+			<div class="small-12 columns">
+				<?php the_content(); ?>
+			</div>
+		</div>	
+
+			
+
+
 		<?php
-			the_content();
+			// wp_link_pages( array(
+			// 	'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+			// 	'after'       => '</div>',
+			// 	'link_before' => '<span>',
+			// 	'link_after'  => '</span>',
+			// 	'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+			// 	'separator'   => '<span class="screen-reader-text">, </span>',
+			// ) );
 
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
-
-			if ( '' !== get_the_author_meta( 'description' ) ) {
-				get_template_part( 'template-parts/biography' );
-			}
+			// if ( '' !== get_the_author_meta( 'description' ) ) {
+			// 	get_template_part( 'template-parts/biography' );
+			// }
 		?>
+
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php twentysixteen_entry_meta(); ?>
+	<!-- <footer class="entry-footer">
+		<?php // twentysixteen_entry_meta(); ?>
 		<?php
 			edit_post_link(
 				sprintf(
@@ -49,5 +111,5 @@
 				'</span>'
 			);
 		?>
-	</footer><!-- .entry-footer -->
+	</footer> --><!-- .entry-footer -->
 </article><!-- #post-## -->
